297ddf2acd7ee80328ffde50d2bc95744b8d5266
KoeppelSoftwareEngineer <MaximKoeppelPower@gmail.com>
2025-12-03 14:44:31 +0100

    Apply Ruff formatting

--- backend/_version.py
+++ backend/_version.py
@@ -27,7 +27,7 @@ version_tuple: VERSION_TUPLE
 commit_id: COMMIT_ID
 __commit_id__: COMMIT_ID
 
-__version__ = version = '0.1.dev17+g6ebd4f70c.d20251203'
-__version_tuple__ = version_tuple = (0, 1, 'dev17', 'g6ebd4f70c.d20251203')
+__version__ = version = "0.1.dev17+g6ebd4f70c.d20251203"
+__version_tuple__ = version_tuple = (0, 1, "dev17", "g6ebd4f70c.d20251203")
 
-__commit_id__ = commit_id = 'g6ebd4f70c'
+__commit_id__ = commit_id = "g6ebd4f70c"
