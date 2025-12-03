e83a98309394189ed63928b5844495dd96d4a5be
KoeppelSoftwareEngineer <MaximKoeppelPower@gmail.com>
2025-12-03 14:42:10 +0100

    Update: backend/_version.py

--- backend/_version.py
+++ backend/_version.py
@@ -11,11 +11,11 @@ __all__ = [
 ]
 
 TYPE_CHECKING = False
-if TYPE_CHECKING:
-    from typing import Tuple, Union
 
+if TYPE_CHECKING:
     VERSION_TUPLE = tuple[int | str, ...]
-    COMMIT_ID = Union[str, None]
+    COMMIT_ID = str | None
+
 else:
     VERSION_TUPLE = object
     COMMIT_ID = object
@@ -27,7 +27,7 @@ version_tuple: VERSION_TUPLE
 commit_id: COMMIT_ID
 __commit_id__: COMMIT_ID
 
-__version__ = version = "0.1.dev2+g6bfb322d1.d20251128"
-__version_tuple__ = version_tuple = (0, 1, "dev2", "g6bfb322d1.d20251128")
+__version__ = version = '0.1.dev17+g6ebd4f70c.d20251203'
+__version_tuple__ = version_tuple = (0, 1, 'dev17', 'g6ebd4f70c.d20251203')
 
-__commit_id__ = commit_id = "g6bfb322d1"
+__commit_id__ = commit_id = 'g6ebd4f70c'
